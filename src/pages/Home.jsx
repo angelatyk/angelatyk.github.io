02a5eaf72ceb5fd07b64e2948d2489f52ad5f141
angelatyk <angelatyk@gmail.com>
2025-07-15 01:30:27 -0400

    Initial commit.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            {/* Navigation */}
+            <main className="min-h-screen">Home Page</main>
+        </>
+    );
+}
+
+export default Home;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+	},
+});
