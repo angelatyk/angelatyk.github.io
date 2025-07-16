66747dd67488fd655e070570dcd0ae6ef9fb9fe8
angelatyk <angelatyk@gmail.com>
2025-07-16 01:47:32 -0400

    Fixed component import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
 
 function App() {
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="*" element={<Navigate to="/" replace />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
