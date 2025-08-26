d753fbf89f7298f431925000914b88c554cb2267
angelatyk <angelatyk@gmail.com>
2025-08-26 00:30:39 -0400

    Initial checkin of Projects page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import NavLink from "../components/NavLink";
 import About from "../pages/About";
 import ContactCTA from "../pages/ContactCTA";
 import Hero from "../pages/Hero";
+import Projects from "../pages/Projects";
 import Skills from "../pages/Skills";
 
 function Home() {
@@ -134,7 +135,7 @@ function Home() {
 						<NavLink link="" label="Home" />
 						<NavLink link="about" label="About" />
 						<NavLink link="skills" label="Skills" />
-						{/* <NavLink link="projects" label="Projects" /> */}
+						<NavLink link="projects" label="Projects" />
 						<NavLink link="/Angela_Kwok_Resume.pdf" label="Resume" isExternal download className="btn-primary" />
 					</ul>
 
@@ -151,7 +152,7 @@ function Home() {
 					<NavLink link="" label="Home" onClick={() => setMenuOpen(false)} />
 					<NavLink link="about" label="About" onClick={() => setMenuOpen(false)} />
 					<NavLink link="skills" label="Skills" onClick={() => setMenuOpen(false)} />
-					{/* <NavLink link="projects" label="Projects" onClick={() => setMenuOpen(false)} /> */}
+					<NavLink link="projects" label="Projects" onClick={() => setMenuOpen(false)} />
 					<NavLink link="/Angela_Kwok_Resume.pdf" label="Resume" isExternal download onClick={() => setMenuOpen(false)} />
 				</ul>
 			)}
@@ -160,8 +161,7 @@ function Home() {
 				<Hero heroRef={heroRef} />
 				<About aboutRef={aboutRef} />
 				<Skills skillsRef={skillsRef} />
-				{/* Projects */}
-				{/* <section id="Projects"></section> */}
+				<Projects projectsRef={projectsRef} />
 				<ContactCTA contactRef={contactRef} />
 			</main>
 
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+function Projects({ projectsRef }) {
+	return (
+		<section className="section-container" id="projects" ref={projectsRef}>
+			<div className="section-content">
+				<h2 className="section-heading">Projects.</h2>
+			</div>
+		</section>
+	);
+}
+
+export default Projects;
