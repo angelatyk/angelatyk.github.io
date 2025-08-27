631b88f6e20552794e7f4cea7b547d7f3e04f072
angelatyk <angelatyk@gmail.com>
2025-08-26 22:43:56 -0400

    Make years of experience be calculated.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,13 @@
 function About({ aboutRef }) {
+	const fulltimeStartDate = new Date(2014, 0, 13); // January 13, 2014
+	const today = new Date();
+
+	let yearsOfExperience = today.getFullYear() - fulltimeStartDate.getFullYear();
+
+	if (today.getMonth() < fulltimeStartDate.getMonth() || (today.getMonth() === fulltimeStartDate.getMonth() && today.getDate() < fulltimeStartDate.getDate())) {
+		yearsOfExperience--;
+	}
+
 	return (
 		<section className="section-container" id="about" ref={aboutRef}>
 			<div className="section-content">
@@ -8,8 +17,8 @@ function About({ aboutRef }) {
 					into place and just works. That spark for creating and problem-solving still drives everything I do.
 				</p>
 				<p>
-					With over 11 years in full-stack development, I’ve honed my expertise in enterprise Java and J2EE technologies, ventured into modern front-end with React and Node.js, and explored the
-					exciting world of AI and machine learning through Python, TensorFlow, Keras, and Scikit-learn.
+					With over {yearsOfExperience} years in full-stack development, I’ve honed my expertise in enterprise Java and J2EE technologies, ventured into modern front-end with React and Node.js, and
+					explored the exciting world of AI and machine learning through Python, TensorFlow, Keras, and Scikit-learn.
 				</p>
 				<p>
 					I’m curious by nature, collaborative by choice, and happiest when I’m learning something new. Whether it’s building a feature, training a model, or brainstorming with a team, I’m always
