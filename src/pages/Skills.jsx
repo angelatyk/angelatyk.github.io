ac23920465b2dd74a679233cddad55367e33299c
angelatyk <angelatyk@gmail.com>
2025-10-03 22:54:52 -0400

    Updated skills and resume.

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,18 +6,18 @@ function Skills({ skillsRef }) {
 				<div className="skills-list">
 					<div className="skill-category">
 						<h3 className="skill-heading">Proficient</h3>
-						<p>Java, Javascript/Typescript, Python, React, Node.js, Express.js, Spring, Hibernate, MySQL, Oracle, RESTful APIs, Git</p>
+						<p>Java, JavaScript/TypeScript, Python, React, Next.js, Node.js, Express.js, Spring, Hibernate, MySQL, Oracle, RESTful APIs, Git, HTML5, CSS3</p>
 					</div>
 					<div className="skill-category">
 						<h3 className="skill-heading">Knowledgeable</h3>
 						<p>
-							TensorFlow, Keras, Scikit-learn, PyTorch, Hugging Face, Pandas, NumPy, Deep Learning, Natural Language Processing, Computer Vision, Reinforcement Learning, Prompt Engineering, Data
-							Preprocessing, Feature Engineering, Model Deployment
+							Pandas, NumPy, Scikit-learn, TensorFlow, Keras, PyTorch, Hugging Face, Deep Learning, Natural Language Processing (NLP), Large Language Models (LLMs), Generative AI, Prompt Engineering,
+							Computer Vision, Reinforcement Learning, Model Deployment
 						</p>
 					</div>
 					<div className="skill-category">
 						<h3 className="skill-heading">Familiar</h3>
-						<p>Selenium, Robot Framework, OAuth, JWT, Heroku</p>
+						<p>Selenium, Robot Framework, OAuth / JWT, Heroku</p>
 					</div>
 				</div>
 			</div>
